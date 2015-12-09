3bc07ad5645b6be6008b4e87ac13d66c15ad4501
Nate Barbettini <nathanaelb@gmail.com>
2015-12-08 20:13:51 -0800

    First version!

---
+++ Fibonacci/Fibonacci.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Fibonacci.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Get_5th_number()
+        {
+            var generator = new FibonacciGenerator();
+
+            Assert.Equal(generator.Fibonacci(5), 5);
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/FibonacciGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace Fibonacci
+{
+    public class FibonacciGenerator
+    {
+        public int Fibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+    }
+}
