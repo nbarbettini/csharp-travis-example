3cc369e766d5866aef7ba3e685ba2e7feff38d77
Nate Barbettini <nathanaelb@gmail.com>
2015-12-08 20:21:55 -0800

    New test

--- Fibonacci/Fibonacci.Tests/Tests.cs
+++ Fibonacci/Fibonacci.Tests/Tests.cs
@@ -11,5 +11,13 @@ namespace Fibonacci.Tests
 
             Assert.Equal(generator.Fibonacci(5), 5);
         }
+
+        [Fact]
+        public void Get_6th_number()
+        {
+            var generator = new FibonacciGenerator();
+
+            Assert.Equal(generator.Fibonacci(6), 8);
+        }
     }
 }
